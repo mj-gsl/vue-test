306ce23668d7e871cd2da93763d103f42d4400c5
mj-gsl <gsl.produktiion@gmail.com>
2024-02-14 12:23:09 +0100

    Fehler Behoben (postgresql verbindung)

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -5,3 +5,4 @@ CREATE TABLE IF NOT EXISTS products (
   price NUMERIC(10, 2) NOT NULL
 );
 CREATE INDEX ON products (name);
+INSERT INTO products (name, description, price) VALUES ('Wild horses', 'Horses, Pair, Wild horses image', 200.99), ('Grafitti', 'Color, Acrylic paint, Art image', 120.99), ('Watercolor', 'colors watercolor .', 109.99);
