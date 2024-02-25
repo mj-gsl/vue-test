4b940944148e080e4e25d16386df9b7adc01a025
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-02-25 20:21:03 +0100

    show the categories in the page

---
+++ my-ecommerce-frontend/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/HomeView.vue";
+// import Category from "../views/CategoryView.vue";
+// import Artwork from "../views/ArtworkView.vue";
+
+const routes = [
+    { path: "/", name: "Home", component: Home },
+    { 
+        path: "/category", 
+        name: "Category", 
+        component: () => import(/*webpackChunkName: "Category" */ "../views/CategoryView.vue") },
+    // {path: "/category/:categoryId", name: "Artwork", component: Artwork}
+    ];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
+
+export default router;
